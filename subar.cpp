ec554f02454ceb6cc01f82ba29041bd4c19e3dc9
anitsacm <35287940+anitsacm@users.noreply.github.com>
2018-01-10 14:37:13 +0530

    Add files via upload

--- subar.cpp
+++ subar.cpp
@@ -1,65 +1,90 @@
 #include <iostream>
+#include <algorithm>
 #include <limits.h>
 using namespace std;
- 
-long max_sub_array(long n, long k) {
-	long sum=0, temp;
-	long prev_sum=0, pos_sum=0;
-	bool flag=true;
-	long neg=LONG_MIN;
-	bool all_neg=true;
- 
-	while(n--) {
-		cin >> temp;
-		if (temp >= 0) {
-			sum+=temp;
 
-			if (all_neg) {
-				all_neg=false;
-			}
+long max_of(long arr[], long n) {
+	long max=arr[0];
 
-		} else {
-			if (neg < temp) {
-				neg=temp;
-			}
-
-			if (flag) {
-				pos_sum=sum;
-			}
- 
-			if (prev_sum < sum) {
-				prev_sum=sum;
-			}
-			sum=0; 
-			flag=false;
+	for (long i=1;i<n;i++) {
+		if ( max < arr[i] ) {
+			max = arr[i];
 		}
 	}
- 
-	if (flag) {
-		return sum*k;
-	}
- 
- 	if (all_neg) {
- 		return neg;
- 	}
- 	
-	sum+=pos_sum;
-	return prev_sum > sum ? prev_sum:sum;
+
+	return max;
 }
- 
+
+long max_sub(long arr[], long n, long k) {
+	long prev_sum, max_sum, pos_sum, sum;
+	prev_sum = max_sum = INT_MIN;
+	sum = 0;
+	long reg = 0;
+	bool first_neg = true;
+
+	for (long i=0;i<n;i++) {
+		if ( arr[i] >= 0 ) {
+			sum+=arr[i];
+			if ( max_sum <= sum ) {
+				max_sum = sum;
+				reg=i;
+			}
+		} else {
+			if ( first_neg ) {
+				pos_sum = sum;
+			}
+
+			if ( max_sum <= sum ) {
+				max_sum = sum;
+				reg=i;
+			}
+
+			prev_sum = sum;
+			sum = 0;
+			first_neg = false;
+		}
+	}
+
+	if (first_neg) {
+		return max_sum*k;
+	} else {
+		if ( max_sum == prev_sum && reg == n-1 ) {
+			return max_sum + pos_sum;
+		} else {
+			return max_sum;
+		}
+	}
+}
+
 int main() {
 	freopen("testcase", "r" , stdin);
 	int t;
 	cin >> t;
  
 	long n, k, result;
+	bool all_neg = true;
 	while(t--) {
 		cin >> n;
 		cin >> k;
- 
-		result=max_sub_array(n,k);
-		cout << result << endl;
+ 		long arr[n];
+
+ 		for (long i=0;i<n;i++) {
+ 			cin >> arr[i];
+
+ 			if ( arr[i] > 0 ) {
+ 				all_neg = false;
+ 			}
+ 		}
+
+ 		if ( all_neg ) {
+ 			result = max_of(arr, n);
+ 		} else {
+ 			result = max_sub(arr, n, k);
+ 		}
+
+ 		cout << result << endl;
+ 		all_neg = true;
 	}
  
 	return 0;
-} 
+}
